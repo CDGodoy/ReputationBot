fdf28900929c7e1793160b771bdb7aa33b9367d8
CDGodoy <cdgodoy17@gmail.com>
2021-07-22 23:37:25 -0300

    Implementando o banco e testando funcoes

--- hello.js
+++ hello.js
@@ -1,70 +1,84 @@
 const TelegramBot = require('node-telegram-bot-api');
+const Database = require('better-sqlite3')
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '1194976667:AAGrxoVyWVWjvVJL4EA2bvjxdglaVwTmytM';
+const db = new Database('meubanco.sqlite', { verbose: console.log });
+// console.log(db)
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+// const insertInto = db.prepare('INSERT INTO users VALUES (?, ?, ?)').run(null, 'ericao', 1231)
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
+const selectAll = db.prepare('SELECT * FROM users').all()
 
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
- 
-  const resp = match[1]; // the captured "whatever"
-  console.log(match)
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+console.log()
+setTimeout(() => {
+	const updateOne = db.prepare('UPDATE users SET reputation  = ? WHERE nome = ?').run(50, 'ericao')
+}, 500)
 
-bot.onText(/\/commands/, (mensagem) => {
-  const chatId = mensagem.chat.id;
+const selectAll = db.prepare('SELECT * FROM users').all()
 
-  texto = "Não tenho comandos ainda fi"
+// const selectOne = db.prepare('SELECT * FROM users WHERE users.nome = ?').get('godoy')
 
-  bot.sendMessage(chatId, texto);
-});
+// console.log(selectRep.sort((a, b) => b.reputation - a.reputation)[0])
+// console.log(selectRep.find(item => item.reputation > 5 && item.nome === 'ericao'))
 
-bot.onText(/\/up (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const nome = msg.from.first_name;
 
-  const resp = match[1]
-  const texto = `A reputação de ${resp} foi aumentada`
+// const updateRep = db.prepare('UPDATE users SET reputation  = ? WHERE userId = ?', [2, '@CDGODOY'])
 
-  console.log(msg, texto)
 
-  bot.sendMessage(chatId, texto);
-});
+// // replace the value below with the Telegram token you receive from @BotFather
+// const token = '1194976667:AAGrxoVyWVWjvVJL4EA2bvjxdglaVwTmytM';
 
-bot.onText(/\/ban (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const nome = msg.from.first_name;
+// // Create a bot that uses 'polling' to fetch new updates
+// const bot = new TelegramBot(token, {
+// 	polling: true
+// });
 
-  const resp = match[1]
-  const texto = `A reputação de ${resp} foi reduzida, seu lixo`
+// /** 
+//  * 
+//  */
+// bot.onText(/\/up (.+)/, (msg, match) => {
+// 	const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, texto);
-});
+// 	const resp = match[1]
 
-bot.onText(/\/comandos/, (msg)=> {
-  const chatId = msg.chat.id;
+// 	const texto = `A reputação de ${resp} foi aumentada`
 
-  const texto = "Meus comandos são: \n \
-  /ban (para reduzir a reputação de um mau usuário) \n \
-  /up  (para aumentar a reputação de um bom usuário \n \
-  /Cadastrar (para cadastrar algum usuário) \n \
-  /rep (para verificar a reputação de alguém)"
+// 	bot.sendMessage(chatId, texto);
+// });
 
-  bot.sendMessage(chatId, texto);
-})
+// /**
+//  * 
+//  */
+// bot.onText(/\/ban (.+)/, (msg, match) => {
+// 	const chatId = msg.chat.id;
 
-bot.on('command', (msg) =>{
-  console.log(msg)
-  const chatId = msg.chat.id;
+// 	const resp = match[1]
+// 	const texto = `A reputação de ${resp} foi reduzida`
 
-  bot.sendMessage(chatId, 'Olá');
-});
+// 	bot.sendMessage(chatId, texto);
+// });
+
+// /**
+//  * 
+//  */
+// bot.onText(/\/comandos/, (msg) => {
+// 	const chatId = msg.chat.id;
+
+// 	const texto = "Meus comandos são: \n \
+//   /ban (para reduzir a reputação de um mau usuário) \n \
+//   /up  (para aumentar a reputação de um bom usuário) \n \
+//   /rep (para verificar a reputação de alguém)"
+
+// 	bot.sendMessage(chatId, texto);
+// });
+
+// /**
+//  * 
+//  */
+// bot.onText(/\/rep (.+)/, (msg, match) => {
+//   const chatId = msg.chat.id
+//   const userName = match[1]
+
+//   let rep = 0 //Vamos ler a reputação de um user no banco
+
+//   bot.sendMessage(chatId, rep)
+// })
